e9fda3ebc532665b6d08636c6bc7a6330e00a7ce
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2020-12-27 21:03:18 +0900

    feat 時計完成

--- js/chartjs/index.js
+++ js/chartjs/index.js
@@ -1,8 +1,12 @@
 function watchRepeat() {
   showClock();
   todayRemain();
+  yearRemain();
+  // planetRemain();
 }
 drawTodayGraph();
+drawYearGraph();
+// drawPlanetGraph();
 
 //時計用桁数調整
 function set2fig(num) {
@@ -102,33 +106,41 @@ function todayRemain() {
 
   return todayRemainRate;
 }
-function todayRemain() {
+
+function yearRemain() {
   var nowTime = new Date();
 
-  var endToday = new Date(
-    nowTime.getFullYear(),
-    nowTime.getMonth(),
-    nowTime.getDate() + 1
-  );
-  var startToday = new Date(
-    nowTime.getFullYear(),
-    nowTime.getMonth(),
-    nowTime.getDate()
-  );
+  var endYear = new Date(nowTime.getFullYear() + 1, 1, 1);
+  var startYear = new Date(nowTime.getFullYear(), 1, 1);
 
-  var todayRemain = endToday.getTime() - nowTime.getTime(); //   1秒あたり995すすむ←？？
+  var yearRemain = endYear.getTime() - nowTime.getTime(); //   1秒あたり995すすむ←？？
 
   document.getElementById("yearRemainSec").innerHTML =
-    parseInt(todayRemain / 1000, 10) + "second";
+    parseInt(yearRemain / 1000, 10) + "second";
 
-  var todayRemainRate =
-    (todayRemain / (endToday.getTime() - startToday.getTime())) * 100; //今日残り何％か
+  var yearRemainRate =
+    (yearRemain / (endYear.getTime() - startYear.getTime())) * 100; //今日残り何％か
 
-  return todayRemainRate;
+  return yearRemainRate;
 }
-function drawTodayGraph() {
-  //chartjsでグラフ上に文字表示させるためのプラグイン
-  var dataLabelPlugin = {
+
+function planetRemain() {
+  var nowTime = new Date();
+
+  var endPlanet = new Date(2135, 9, 22);
+
+  var PlanetRemain = endPlanet.getTime() - nowTime.getTime(); //   1秒あたり995すすむ←？？
+
+  document.getElementById("planetRemainSec").innerHTML =
+    parseInt(PlanetRemain / 1000, 10) + "second";
+
+  // var planetRemainRate = (planetRemain / endPlanet.getTime()) * 100; //今日残り何％か
+
+  // return planetRemainRate;
+}
+
+function showLavel() {
+  return (dataLabelPlugin = {
     afterDatasetsDraw: function (chart, easing) {
       // To only draw at the end of animation, check for easing === 1
       var ctx = chart.ctx;
@@ -159,7 +171,7 @@ function drawTodayGraph() {
             var padding = 5;
             var position = element.tooltipPosition();
             ctx.fillText(
-              dataString,
+              dataString + "%",
               position.x,
               position.y - fontSize / 2 - padding
             );
@@ -167,7 +179,12 @@ function drawTodayGraph() {
         }
       });
     },
-  };
+  });
+}
+
+function drawTodayGraph() {
+  //chartjsでグラフ上に文字表示させるためのプラグイン
+  dataLabelPlugin = showLavel();
   //グラフ描写
   var todayRemainRate = parseInt(todayRemain(), 10);
   var todayPercentCircleId = document.getElementById("todayPercentCircle");
@@ -187,9 +204,53 @@ function drawTodayGraph() {
     plugins: [dataLabelPlugin],
   });
 }
+function drawYearGraph() {
+  //chartjsでグラフ上に文字表示させるためのプラグイン
+  dataLabelPlugin = showLavel();
+  //グラフ描写
+  var yearRemainRate = parseInt(yearRemain(), 10);
+  var yearPercentCircleId = document.getElementById("yearPercentCircle");
+  var yearPercentCircle = new Chart(yearPercentCircleId, {
+    type: "pie",
+    data: {
+      labels: ["経過", "残り"],
+      datasets: [
+        {
+          label: { fontColor: "Special Elite" },
+          data: [100 - yearRemainRate, yearRemainRate],
+          backgroundColor: ["#3F2B36", "#E98B2A"],
+        },
+      ],
+    },
+    options: { animation: { animateScale: true } },
+    plugins: [dataLabelPlugin],
+  });
+}
 
-function todayView() {}
+function drawPlanetGraph() {
+  //chartjsでグラフ上に文字表示させるためのプラグイン
+  dataLabelPlugin = showLavel();
+  //グラフ描写
+  var planetRemainRate = parseInt(planetRemain(), 10);
+  var planetPercentCircleId = document.getElementById("planetPercentCircle");
+  var planetPercentCircle = new Chart(planetPercentCircleId, {
+    type: "pie",
+    data: {
+      labels: ["経過", "残り"],
+      datasets: [
+        {
+          label: { fontColor: "Special Elite" },
+          data: [100 - planetRemainRate, planetRemainRate],
+          backgroundColor: ["#3F2B36", "#E98B2A"],
+        },
+      ],
+    },
+    options: { animation: { animateScale: true } },
+    plugins: [dataLabelPlugin],
+  });
+}
 setInterval("watchRepeat()", 1000);
 setInterval("drawTodayGraph()", 60000);
-setInterval("drawYearGraph()", 60000);
+setInterval("drawYearGraph()", 86400000);
+// setInterval("drawPlanetGraph()", 86400000);
 // ここまで
