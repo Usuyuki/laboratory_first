b2ab8269277b36254845084df62b9dfca42c084f
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-02-23 08:49:15 +0900

    wip Swiperうまくいかない

--- js/swiper/swiper.js
+++ js/swiper/swiper.js
@@ -1,1 +1,11 @@
-var mySwiper = new Swiper(".swiper-container", {});
+var mySwiper = new Swiper(".swiper-container", {
+  effect: "flip",
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
